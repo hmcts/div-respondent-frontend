05de71613c235b3ce9e6cd7979adcf53dc60b5b0
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2018-08-14 12:49:02 +0100

    lint fixes

--- test/unit/middleware/setLocalsMiddleware.test.js
+++ test/unit/middleware/setLocalsMiddleware.test.js
@@ -4,15 +4,14 @@ const { idamLoggedin } = require(modulePath);
 const { sinon, expect } = require('@hmcts/one-per-page-test-suite');
 
 describe(modulePath, () => {
-
   it('checks if user is logged in', () => {
     const next = sinon.stub();
-    const res = { 
-      locals : {
-        isLoggedIn : true
+    const res = {
+      locals: {
+        isLoggedIn: true
       }
-    }
-    const req = { idam : {} };
+    };
+    const req = { idam: {} };
     idamLoggedin(req, res, next);
     expect(next.calledOnce).to.eql(true);
   });
