0d0afad46916ed74b50242538f67f299505f9210
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2018-08-17 15:08:07 +0100

    fix for unit test for respond step

--- test/unit/steps/respond.test.js
+++ test/unit/steps/respond.test.js
@@ -1,11 +1,11 @@
 const modulePath = 'steps/respond/Respond.step';
 
 const Respond = require(modulePath);
-// const ReviewApplication = require('steps/review-application/ReviewApplication.step');
+const ReviewApplication = require('steps/review-application/ReviewApplication.step');
 const idam = require('services/idam');
-// const { middleware, interstitial, sinon, content } = require('@hmcts/one-per-page-test-suite');
-const { middleware, sinon, content } = require('@hmcts/one-per-page-test-suite');
+const { middleware, interstitial, sinon, content } = require('@hmcts/one-per-page-test-suite');
 const { getUserData } = require('middleware/ccd');
+const getSteps = require('steps');
 
 describe(modulePath, () => {
   beforeEach(() => {
@@ -19,11 +19,11 @@ describe(modulePath, () => {
   it('has idam.protect middleware', () => {
     return middleware.hasMiddleware(Respond, [ idam.protect(), getUserData ]);
   });
-  /*
+
   it('redirects to next page', () => {
-    return interstitial.navigatesToNext(Respond, ReviewApplication);
+    return interstitial.navigatesToNext(Respond, ReviewApplication, getSteps());
   });
-  */
+
   it('renders the content', () => {
     return content(Respond);
   });
