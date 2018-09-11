40fbcacfea469542c6ddf13a7dabf6690a3ab0e3
Hosain <hosain.alahmad@hmcts.net>
2018-09-11 17:22:54 +0100

    DIV-3113 - attempt to fix idam login in e2e test in CI
    When config.features.idam is enabled (CI), we login with a proper user

--- test/e2e/pages/idam.step.js
+++ test/e2e/pages/idam.step.js
@@ -1,17 +1,28 @@
+const IdamLoginPage = require('mocks/steps/idamLogin/IdamLogin.step');
 const commonContent = require('common/content');
 const content = require('mocks/steps/idamLogin/IdamLogin.content');
+const config = require('config');
+const idamConfigHelper = require('test/e2e/helpers/idamConfigHelper.js');
 
 function seeIdamLoginPage() {
   const I = this;
-
   I.see(content.en.title);
 }
 
 function login() {
   const I = this;
 
-  I.click(content.en.fields.success.yes);
-  I.click(commonContent.en.continue);
+  if (config.features.idam) {
+    I.seeInCurrentUrl('/login?');
+    I.fillField('username', idamConfigHelper.getTestEmail());
+    I.fillField('password', idamConfigHelper.getTestPassword());
+    I.navByClick('Sign in');
+    I.wait(2);
+  } else {
+    I.seeCurrentUrlEquals(IdamLoginPage.path);
+    I.click(content.en.fields.success.yes);
+    I.click(commonContent.en.continue);
+  }
 }
 
 module.exports = {
