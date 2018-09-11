f3214e3eefeb4781006224ff8fd5fdb58ca66b53
Hosain <hosain.alahmad@hmcts.net>
2018-09-11 15:42:46 +0100

    DIV-3113 - updated one-per-page and one-per-page-test-suite
    * Ignore top-level content fields not used by the template directly
    * Added testErrors to the ChooseAResponse page
    * Added problem_with_this_page to the base page/question template

--- test/unit/steps/chooseAResponse.test.js
+++ test/unit/steps/chooseAResponse.test.js
@@ -24,11 +24,11 @@ describe(modulePath, () => {
     return question.redirectWithField(ChooseAResponse, fields, End);
   });
 
-  it('does not proceed when answer is not provided', () => {
-    return question.redirectWithField(ChooseAResponse, {}, ChooseAResponse);
+  it('shows error if question is not answered', () => {
+    return question.testErrors(ChooseAResponse);
   });
 
   it('renders the content', () => {
-    return content(ChooseAResponse);
+    return content(ChooseAResponse, {}, { ignoreContent: ['info'] });
   });
 });
