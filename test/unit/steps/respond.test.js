caaf1df2a032a680f7a33f3a04990f9f55acfb3a
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2018-09-13 12:23:20 +0100

    update w3cjs test and respond step unit test

--- test/unit/steps/respond.test.js
+++ test/unit/steps/respond.test.js
@@ -4,22 +4,20 @@ const Respond = require(modulePath);
 const ReviewApplication = require('steps/review-application/ReviewApplication.step');
 const idam = require('services/idam');
 const { middleware, interstitial, sinon, content } = require('@hmcts/one-per-page-test-suite');
-const ccd = require('middleware/ccd');
 const getSteps = require('steps');
-const CONF = require('config');
+const request = require('request-promise-native');
+const rawResponse = require('resources/raw-response-mock.json');
 
 describe(modulePath, () => {
-  const defaultEnvironment = CONF.environment;
   beforeEach(() => {
-    CONF.environment = 'development';
+    sinon.stub(request, 'get')
+      .resolves(rawResponse);
     sinon.stub(idam, 'protect').returns(middleware.nextMock);
-    sinon.stub(ccd, 'getUserData').callsFake(middleware.nextMock);
   });
 
   afterEach(() => {
-    CONF.environment = defaultEnvironment;
     idam.protect.restore();
-    ccd.getUserData.restore();
+    request.get.restore();
   });
 
   it('has idam.protect middleware', () => {
--- test/w3cjs/index.js
+++ test/w3cjs/index.js
@@ -1,10 +1,13 @@
 const w3cjs = require('w3cjs');
 const steps = require('steps')();
-const { custom, expect } = require('@hmcts/one-per-page-test-suite');
+const { sinon, custom, expect } = require('@hmcts/one-per-page-test-suite');
 const resolveTemplate = require('@hmcts/one-per-page/src/middleware/resolveTemplate');
 const httpStatus = require('http-status-codes');
 const fs = require('fs');
 const path = require('path');
+
+const request = require('request-promise-native');
+const rawResponse = require('resources/raw-response-mock.json');
 
 // Get the mocked session from file
 const filePath = path.join(__dirname, '../../resources/mock.json');
@@ -75,6 +78,9 @@ steps
       let warnings = [];
 
       before(() => {
+        sinon.stub(request, 'get')
+          .resolves(rawResponse);
+
         return stepHtml(step)
           .then(html => w3cjsValidate(html))
           .then(results => {
@@ -84,6 +90,10 @@ steps
           .catch(error => {
             expect(error).to.eql(false, `Error with WC3 module: ${error}`);
           });
+      });
+
+      after(() => {
+        request.get.restore();
       });
 
       it('should not have any html errors', () => {
