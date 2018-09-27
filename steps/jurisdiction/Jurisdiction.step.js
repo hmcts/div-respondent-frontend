66a45add6ea631c48d8236076f1586cfbd82e247
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-09-27 20:16:38 +0100

    Jurisdiction

--- steps/jurisdiction/Jurisdiction.step.js
+++ steps/jurisdiction/Jurisdiction.step.js
@@ -2,18 +2,21 @@ const { Question } = require('@hmcts/one-per-page/steps');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const { goTo } = require('@hmcts/one-per-page/flow');
 const { form, text, object, errorFor } = require('@hmcts/one-per-page/forms');
-const { getUserData } = require('middleware/ccd');
 const Joi = require('joi');
 const idam = require('services/idam');
 const config = require('config');
-const content = require('./LegalProceedings.content');
+const content = require('./Jurisdiction.content');
 
 const yes = 'yes';
 const no = 'no';
 
-class LegalProceedings extends Question {
+class Jurisdiction extends Question {
   static get path() {
-    return config.paths.legalProceedings;
+    return config.paths.jurisdiction;
+  }
+
+  get session() {
+    return this.req.session;
   }
 
   get form() {
@@ -22,8 +25,16 @@ class LegalProceedings extends Question {
       .valid(answers)
       .required();
 
-    const validateCaseDetails = ({ exists = '', details = '' }) => {
-      if (exists === 'yes' && !details.trim().length) {
+    const validateDisagreeReason = ({ agree = '', reason = '' }) => {
+      if (agree === 'no' && !reason.trim().length) {
+        return false;
+      }
+
+      return true;
+    };
+
+    const validateCountry = ({ agree = '', country = '' }) => {
+      if (agree === 'no' && !country.trim().length) {
         return false;
       }
 
@@ -31,34 +42,54 @@ class LegalProceedings extends Question {
     };
 
     const fields = {
-      exists: text.joi(this.content.errors.required, validAnswers),
-      details: text
+      agree: text.joi(this.content.errors.required, validAnswers),
+      reason: text,
+      country: text
     };
 
-    const legalProceedings = object(fields)
+    const jurisdiction = object(fields)
       .check(
-        errorFor('details', this.content.errors.requireCaseDetails),
-        validateCaseDetails
+        errorFor('reason', this.content.errors.reasonRequired),
+        validateDisagreeReason
+      )
+      .check(
+        errorFor('country', this.content.errors.countryRequired),
+        validateCountry
       );
 
-    return form({ legalProceedings });
+    return form({ jurisdiction });
   }
 
   answers() {
-    const question = content.en.cya.question;
-    return answer(this, {
-      question,
-      answer: this.fields.legalProceedings.exists.value === yes ? this.fields.legalProceedings.details.value : content.en.fields.no.answer
-    });
+    const answers = [];
+
+    answers.push(answer(this, {
+      question: content.en.cya.agree,
+      answer: this.fields.jurisdiction.agree.value === yes ? content.en.fields.agree.answer : content.en.fields.disagree.answer
+    }));
+
+    if (this.fields.jurisdiction.agree.value === no) {
+      answers.push(answer(this, {
+        question: content.en.cya.reason,
+        answer: this.fields.jurisdiction.reason.value
+      }));
+
+      answers.push(answer(this, {
+        question: content.en.cya.country,
+        answer: this.fields.jurisdiction.country.value
+      }));
+    }
+
+    return answers;
   }
 
   get middleware() {
-    return [...super.middleware, idam.protect(), getUserData];
+    return [...super.middleware, idam.protect()];
   }
 
   next() {
-    return goTo(this.journey.steps.CheckYourAnswers);
+    return goTo(this.journey.steps.LegalProceedings);
   }
 }
 
-module.exports = LegalProceedings;
+module.exports = Jurisdiction;
