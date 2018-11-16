2e0f05716a04f2bbb6eb5cbee1f31e5c01c68ed4
John O'Neill <j.oneill@kainos.com>
2018-11-16 20:27:59 +0000

    Updated dependencies and updated files for a11y

---
+++ steps/cookies-policy/CookiesPolicy.step.js
@@ -0,0 +1,18 @@
+const { Page } = require('@hmcts/one-per-page');
+const config = require('config');
+
+class CookiesPolicy extends Page {
+  static get ignorePa11yWarnings() {
+    return ['WCAG2AA.Principle1.Guideline1_3.1_3_1.H48'];
+  }
+
+  static get path() {
+    return config.paths.cookiesPolicy;
+  }
+
+  get middleware() {
+    return [...super.middleware];
+  }
+}
+
+module.exports = CookiesPolicy;
