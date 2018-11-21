9c0046e3224476ee64e0e51db39459213d7249c8
Hosain <hosain.alahmad@hmcts.net>
2018-11-21 15:54:53 +0000

    DIV-3494 - remove _beforeStep waiting

--- test/e2e/helpers/JSWait.js
+++ test/e2e/helpers/JSWait.js
@@ -1,14 +1,4 @@
 class JSWait extends codecept_helper { // eslint-disable-line camelcase
-  _beforeStep(step) {
-    const helper = this.helpers.WebDriverIO || this.helpers.Puppeteer;
-
-    // Wait for content to load before checking URL
-    if (step.name === 'seeCurrentUrlEquals' || step.name === 'seeInCurrentUrl') {
-      return helper.waitForElement('#content', 30);
-    }
-    return Promise.resolve();
-  }
-
   async navByClick(text, locator) {
     const helper = this.helpers.WebDriverIO || this.helpers.Puppeteer;
     const helperIsPuppeteer = this.helpers.Puppeteer;
