f44d5506c006aeb68305c59e24486031c38863e6
Hosain <hosain.alahmad@hmcts.net>
2018-11-23 10:49:30 +0000

    DIV-3494 - moved env variable for jenkins to output.tf and added vault uri

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,8 +2,16 @@ output "vaultName" {
     value = "${local.vaultName}"
 }
 
+output "vaultUri" {
+  value = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
+}
+
 output "idam_api_url" {
   value = "${var.idam_api_url}"
+}
+
+output "case_maintenance_base_url" {
+  value = "${local.div_cms_url}"
 }
 
 output "feature_idam" {
