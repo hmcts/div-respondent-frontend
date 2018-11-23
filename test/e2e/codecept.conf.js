c429663204f6b13a6c98ace035a1b370c680bb72
Hosain <hosain.alahmad@hmcts.net>
2018-11-23 15:47:56 +0000

    DIV-3494 - add example local-aat.yml for running tests against aat

--- test/e2e/codecept.conf.js
+++ test/e2e/codecept.conf.js
@@ -25,7 +25,7 @@ exports.config = {
       url: config.tests.e2e.url || config.node.baseUrl,
       waitForTimeout,
       waitForAction,
-      show: false,
+      show: config.tests.e2e.show,
       chrome: {
         ignoreHTTPSErrors: true,
         args: [
