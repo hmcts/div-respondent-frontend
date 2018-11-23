2775a94c9399b887287b794204dd1bde7b0d0186
Hosain <hosain.alahmad@hmcts.net>
2018-11-23 11:31:09 +0000

    DIV-3494 - set env variables once in Jenkinsfile for pipeline

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -6,22 +6,6 @@ output "vaultUri" {
   value = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
 }
 
-output "idam_api_url" {
-  value = "${var.idam_api_url}"
-}
-
-output "base_url" {
-  value = "${local.base_url}"
-}
-
-output "case_maintenance_base_url" {
-  value = "${local.div_cms_url}"
-}
-
-output "feature_idam" {
-  value = "${var.feature_idam}"
-}
-
 output "reform_environment" {
   value = "${var.env}"
 }
