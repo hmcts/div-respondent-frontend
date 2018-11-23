b532e7e5a3396e939a97af617f5020147409d268
Hosain <hosain.alahmad@hmcts.net>
2018-11-23 17:47:30 +0000

    DIV-3494 - update unit test with case-orchestration

--- test/unit/services/caseOrchestration.test.js
+++ test/unit/services/caseOrchestration.test.js
@@ -34,7 +34,7 @@ describe(modulePath, () => {
             headers: {
               Authorization: 'Bearer test'
             },
-            uri: 'http://localhost:3001/link-respondent/1234567890123456/1234'
+            uri: 'http://localhost:3001/case-orchestration/link-respondent/1234567890123456/1234'
           });
       })
       .then(done, done);
