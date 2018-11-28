1cbae878c9e925e97da09150c3ca59053db5146c
Hosain <hosain.alahmad@hmcts.net>
2018-11-28 12:10:30 +0000

    DIV-3494 - changed click to navByClick to minimise flakyness in pipeline

--- test/e2e/paths/happyPath.js
+++ test/e2e/paths/happyPath.js
@@ -17,32 +17,32 @@ Scenario('@Pipeline Proceed with divorce with linked user', async I => {
 
   I.seeRespondPage();
   I.wait(5);
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeReviewApplicationPage();
   I.acknowledgeApplication();
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeChooseAResponsePage();
   I.chooseToProceedWithDivorce();
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeJurisdictionPage();
   I.chooseAgreeToJurisdiction();
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeLegalProceedingPage();
   I.chooseNoLegalProceedings();
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeAgreeToPayCostsPage();
   I.wait(5);
   I.chooseAgreeToPay();
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeContactDetailsPage();
   I.consentToSendingNotifications();
-  I.click(content.en.continue);
+  I.navByClick(content.en.continue);
 
   I.seeCheckYourAnswersPage();
   I.confirmInformationIsTrue();
