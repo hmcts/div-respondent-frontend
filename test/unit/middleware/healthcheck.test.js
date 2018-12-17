e2e7c1008883418d50e3cbeaa5587e0dbaf2236b
Alex Wakeman <wakemana@gmail.com>
2018-12-17 09:09:45 +0000

    Revert health-check to original before using the proxy

--- test/unit/middleware/healthcheck.test.js
+++ test/unit/middleware/healthcheck.test.js
@@ -1,12 +1,10 @@
 const modulePath = 'middleware/healthcheck';
 
-let setupHealthChecks = require(modulePath);
+const setupHealthChecks = require(modulePath);
 const path = require('path');
-const proxyquire = require('proxyquire');
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 const logger = require('@hmcts/nodejs-logging')
-  .Logger
-  .getLogger(path.resolve('middleware/healthcheck.js'));
+  .Logger.getLogger(path.resolve('middleware/healthcheck.js'));
 const { sinon } = require('@hmcts/one-per-page-test-suite');
 const config = require('config');
 const outputs = require('@hmcts/nodejs-healthcheck/healthcheck/outputs');
@@ -15,20 +13,11 @@ const redis = require('services/redis');
 
 const app = {};
 let res = {};
-const requestSuccessObj = () => new Promise(resolve => {
-  return resolve('{ "status": "UP" }');
-});
-const requestErrorObj = () => new Promise((resolve, reject) => {
-  // eslint-disable-next-line prefer-promise-reject-errors
-  return reject('error');
-});
-
 
 describe(modulePath, () => {
   beforeEach(() => {
     app.use = sinon.stub();
-    sinon.stub(redis, 'ping')
-      .resolves('PONG');
+    sinon.stub(redis, 'ping').resolves('PONG');
     sinon.stub(healthcheck, 'web');
     sinon.stub(healthcheck, 'raw');
     sinon.stub(healthcheck, 'status');
@@ -74,42 +63,39 @@ describe(modulePath, () => {
       .then(done, done);
   });
 
-  it('performs healthcheck for idam-login-page', () => {
-    setupHealthChecks = proxyquire(modulePath, { 'request-promise-native': requestSuccessObj });
-    setupHealthChecks(app);
-
-    const idamCallback = healthcheck.raw.secondCall.args[0];
-    idamCallback().then(() => {
-      sinon.assert.calledWith(healthcheck.status, true);
-    });
-  });
-
-  it('throws an error if healthcheck fails for idam-login-page', () => {
-    setupHealthChecks = proxyquire(modulePath, { 'request-promise-native': requestErrorObj });
-    setupHealthChecks(app);
-
-    const idamCallback = healthcheck.raw.secondCall.args[0];
-    idamCallback('error').then(() => {
-      sinon.assert.calledWith(logger.error, 'Health check failed on idam-authentication: error');
-      sinon.assert.calledWith(healthcheck.status, false);
-    });
-  });
-
-  it('throws an error if healthcheck fails for idam-api', () => {
-    setupHealthChecks(app);
-
-    const idamCallback = healthcheck.web.firstCall.args[1].callback;
-    idamCallback('error');
-
-    sinon.assert.calledWith(logger.error, 'Health check failed on idam-api: error');
-  });
-
-  it('performs health-check for idam-api', () => {
+  it('returns up if healthcheck passes for idam-login-page', () => {
     setupHealthChecks(app);
 
     const idamCallback = healthcheck.web.firstCall.args[1].callback;
     idamCallback(null, res);
 
     sinon.assert.called(outputs.up);
+  });
+
+  it('throws an error if healthcheck fails for idam-login-page', () => {
+    setupHealthChecks(app);
+
+    const idamCallback = healthcheck.web.firstCall.args[1].callback;
+    idamCallback('error');
+
+    sinon.assert.calledWith(logger.error, 'Health check failed on idam-login-page: error');
+  });
+
+  it('throws an error if healthcheck fails for idam-api', () => {
+    setupHealthChecks(app);
+
+    const idamCallback = healthcheck.web.secondCall.args[1].callback;
+    idamCallback('error');
+
+    sinon.assert.calledWith(logger.error, 'Health check failed on idam-api: error');
+  });
+
+  it('returns up if healthcheck passes for idam-api', () => {
+    setupHealthChecks(app);
+
+    const idamCallback = healthcheck.web.secondCall.args[1].callback;
+    idamCallback('error');
+
+    sinon.assert.calledWith(logger.error, 'Health check failed on idam-api: error');
   });
 });
