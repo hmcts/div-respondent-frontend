75f55539616f2ae0332c9d2fb6e780a44c6355fb
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-01-24 00:33:07 +0000

    Update codecept.smoke.conf.js

--- test/e2e/codecept.smoke.conf.js
+++ test/e2e/codecept.smoke.conf.js
@@ -1,16 +1,15 @@
-/* eslint-disable */
+/* eslint-disable no-process-env */
 const config = require('config');
 
 const waitForTimeout = '10000';
-const waitForAction = '3000';
-const chromeArgs = [ '--no-sandbox' ];
+const waitForAction = '2000';
 
 exports.config = {
   tests: './smoke/*.js',
-  output: config.tests.functional.outputDir,
+  output: config.tests.e2e.outputDir,
   helpers: {
     Puppeteer: {
-      url: config.tests.functional.url || config.node.baseUrl,
+      url: config.tests.e2e.url || config.node.baseUrl,
       waitForTimeout,
       waitForAction,
       show: false,
@@ -19,7 +18,9 @@ exports.config = {
       keepBrowserState: false,
       chrome: {
         ignoreHTTPSErrors: true,
-        args: chromeArgs
+        args: [
+          '--no-sandbox'
+        ]
       }
     },
     JSWait: { require: './helpers/JSWait.js' }
@@ -32,7 +33,7 @@ exports.config = {
       },
       'mocha-junit-reporter': {
         stdout: '-',
-        options: { mochaFile: `${config.tests.functional.outputDir}/smoke-result.xml` }
+        options: { mochaFile: `${config.tests.e2e.outputDir}/smoke-result.xml` }
       }
     }
   },
