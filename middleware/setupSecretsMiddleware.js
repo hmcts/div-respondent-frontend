2fb8ed90589fef4a2121bf46841370ddbbce4087
Qiang Zhou <qiang.zhou@hmcts.net>
2019-03-29 00:41:06 +0000

    Added log

--- middleware/setupSecretsMiddleware.js
+++ middleware/setupSecretsMiddleware.js
@@ -1,5 +1,6 @@
 const config = require('config');
 const { get, set } = require('lodash');
+const logger = require('services/logger').getLogger(__filename);
 
 const setSecret = (secretPath, configPath) => {
   // Only overwrite the value if the secretPath is defined
@@ -10,10 +11,16 @@ const setSecret = (secretPath, configPath) => {
 
 const setup = (req, res, next) => {
   // Property Volumes will be the correct format to use in the future, so it will take precendence
+  const configStr = JSON.stringify(config);
+
   if (config.has('secrets.div')) {
+    logger.infoWithReq(req, 'setupConfig', 'Setted', configStr);
+
     setSecret('secrets.div.session-secret', 'session.secret');
     setSecret('secrets.div.redis-secret', 'services.redis.encryptionAtRestKey');
     setSecret('secrets.div.idam-secret', 'services.idam.secret');
+  } else {
+    logger.infoWithReq(req, 'setupConfig', 'Not setted', configStr);
   }
 
   next();
