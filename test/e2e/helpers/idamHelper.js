30b253fd062237c264ca649e6f449c1e1b387fa0
Hosain <hosainnet@users.noreply.github.com>
2019-04-11 11:45:14 +0100

    DIV-4665 - renamed petitioner to applicant in co-respondent review page (#280)

--- test/e2e/helpers/idamHelper.js
+++ test/e2e/helpers/idamHelper.js
@@ -56,7 +56,7 @@ class IdamHelper extends Helper {
           null,
           'idam_error',
           'Unable to create IDAM test user/token',
-          error.message
+          error
         );
         throw error;
       });
