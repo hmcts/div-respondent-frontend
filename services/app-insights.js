9f4a9bd1ca94f50a62cd82c432ae175a53f472f9
Qiang Zhou <qiang.zhou@hmcts.net>
2019-07-15 17:29:15 +0100

    Fix request logging

--- services/app-insights.js
+++ services/app-insights.js
@@ -4,9 +4,13 @@ const appInsights = require('applicationinsights');
 const start = () => {
   if (config.services.applicationInsights.instrumentationKey) {
     appInsights.setup(config.services.applicationInsights.instrumentationKey)
-      .setAutoCollectConsole(true, true)
-      .start();
+      .setAutoCollectConsole(true, true);
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'div-rfe';
+
+    appInsights.defaultClient.addTelemetryProcessor(() => {
+      return true;
+    });
+    appInsights.start();
   }
 };
 
