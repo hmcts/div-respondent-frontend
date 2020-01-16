7ab05c0444ad95bfe5a5a0bb3f7107815eab036a
vianvi <30834324+vianvi@users.noreply.github.com>
2020-01-16 11:38:13 +0000

    generate report and don't fail on nightly pipeline

--- security.sh
+++ security.sh
@@ -25,7 +25,7 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   cp *.html functional-output/
   cp activescanReport.xml functional-output/
 
-  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+  curl http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.* functional-output/
 
 
@@ -42,4 +42,4 @@ echo "./audit.json"
 echo
 echo and commit the change
 
-zap-cli -p 1001 alerts -l High
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
