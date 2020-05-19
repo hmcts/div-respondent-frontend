829cc7b89255a9562aa8380d6f5c323f940a62a9
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-19 12:29:51 +0100

    WLA-97: Update checkWelshToggle.js

--- middleware/checkWelshToggle.js
+++ middleware/checkWelshToggle.js
@@ -2,12 +2,13 @@
 
 const FeatureToggle = require('core/utils/featureToggle');
 
-const featureToggle = new FeatureToggle();
-
 const checkWelshToggle = (req, res, next) => {
+  if (!res.locals.featureToggle) {
+    res.locals.featureToggle = new FeatureToggle();
+  }
   res.locals.launchDarkly = {};
 
-  return featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_welsh', featureToggle.toggleFeature);
+  return res.locals.featureToggle.callCheckToggle(req, res, next, res.locals.launchDarkly, 'ft_welsh', res.locals.featureToggle.toggleFeature);
 };
 
 module.exports = checkWelshToggle;
