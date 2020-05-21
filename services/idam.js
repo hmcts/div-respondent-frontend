2840ecdc4545cab6952811a83c8ef416e0fb95a2
Luca <luca.delbuono@gmail.com>
2020-05-21 21:48:04 +0100

    WLA-97: Add IdAM logging

--- services/idam.js
+++ services/idam.js
@@ -1,6 +1,7 @@
 const idamExpressMiddleware = require('@hmcts/div-idam-express-middleware');
 const idamExpressMiddlewareMock = require('mocks/services/idam');
 const config = require('@hmcts/properties-volume').addTo(require('config'));
+const logger = require('services/logger').getLogger(__filename);
 
 const idamArgs = {
   indexUrl: config.paths.index,
@@ -19,6 +20,14 @@ const setArgsFromRequest = req => {
   // clone args so we don't modify the global idamArgs
   const args = Object.assign({}, idamArgs);
   args.hostName = req.hostname;
+  logger.infoWithReq(req, 'req_session', 'req.session:', req.session);
+  if (req.session && req.session.language) {
+    logger.infoWithReq(req, 'req_session_language', 'req.session.language:', req.session.language);
+  }
+  logger.infoWithReq(req, 'req_query', 'req.query:', req.query);
+  if (req.query && req.query.locale) {
+    logger.infoWithReq(req, 'req_query_locale', 'req.query.locale:', req.query.locale);
+  }
   args.language = (req.session && req.session.language) ? req.session.language : 'en';
   args.redirectUri = `https://${req.get('host') + config.paths.authenticated}`;
   return args;
