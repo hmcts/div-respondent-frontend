9b390c9ae265822230ff07a758260886e03e4631
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-07-13 16:19:12 +0100

    Fix Equality path

--- test/e2e/pages/common/equality.js
+++ test/e2e/pages/common/equality.js
@@ -1,9 +1,10 @@
-const Equality = require('steps/equality/Equality.step');
+// This will need to change once the PCQ-Divorce PR is merged into master
+const path = 'https://pcq.aat.platform.hmcts.net/offline';
 
 function seeEqualityPage() {
   const I = this;
-  I.waitInUrl(Equality.path, 15);
-  I.seeCurrentUrlEquals(Equality.path);
+  I.waitInUrl(path, 15);
+  I.seeCurrentUrlEquals(path);
   I.waitForText('Equality and diversity questions', 5);
 }
 
