46560fde190ad0ccbfe73e6c6faf18ea312b2e6a
gurarach <gurdeep.araich@hmcts.net>
2020-08-03 21:51:30 +0100

    unit test fixed with all 3 redirects tested

--- test/unit/steps/respondent/languagePreference.test.js
+++ test/unit/steps/respondent/languagePreference.test.js
@@ -1,7 +1,8 @@
 const modulePath = 'steps/respondent/language-preference/LanguagePreference.step';
 const LanguagePreference = require(modulePath);
-//  const ChooseAResponse = require('steps/respondent/choose-a-response/ChooseAResponse.step');
+const ChooseAResponse = require('steps/respondent/choose-a-response/ChooseAResponse.step');
 const AdmitAdultery = require('steps/respondent/admit-adultery/AdmitAdultery.step');
+const ConsentDecree = require('steps/respondent/consent-decree/ConsentDecree.step');
 const idam = require('services/idam');
 const checkWelshToggle = require('middleware/checkWelshToggle');
 const { middleware, question, sinon } = require('@hmcts/one-per-page-test-suite');
@@ -14,6 +15,8 @@ const defaultSession = {
 };
 
 describe(modulePath, () => {
+  const session = {};
+
   beforeEach(() => {
     sinon.stub(idam, 'protect').returns(middleware.nextMock);
   });
@@ -43,8 +46,21 @@ describe(modulePath, () => {
     return question.answers(LanguagePreference, stepData, expectedContent, defaultSession);
   });
 
-  it('redirects to next page', () => {
+  it('redirects to admit adultery page if case is adultery', () => {
     const fields = { languagePreferenceWelsh: 'Yes' };
-    return question.redirectWithField(LanguagePreference, fields, AdmitAdultery);
+    session.originalPetition = { reasonForDivorce: 'adultery' };
+    return question.redirectWithField(LanguagePreference, fields, AdmitAdultery, session);
+  });
+
+  it('redirects to consent decree page if case is 2 year separation', () => {
+    const fields = { languagePreferenceWelsh: 'Yes' };
+    session.originalPetition = { reasonForDivorce: 'separation-2-years' };
+    return question.redirectWithField(LanguagePreference, fields, ConsentDecree, session);
+  });
+
+  it('redirects to choose a response page if reason is not adultery or 2 year separation', () => {
+    const fields = { languagePreferenceWelsh: 'No' };
+    session.originalPetition = { reasonForDivorce: 'separation-5-years' };
+    return question.redirectWithField(LanguagePreference, fields, ChooseAResponse, session);
   });
 });
