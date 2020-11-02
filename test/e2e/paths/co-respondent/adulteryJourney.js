68fc52a46975b4a69e538030f97b9fa03ef0e43c
Mariana <maryanasoad@hotmail.com>
2020-11-02 14:03:43 +0000

    Adultery co-respondent E2E journey, happy path

--- test/e2e/paths/co-respondent/adulteryJourney.js
+++ test/e2e/paths/co-respondent/adulteryJourney.js
@@ -1,13 +1,23 @@
 const content = require('common/content');
+const config = require('config');
+const adulteryDivorceSession = require('test/resources/corespondent-divorce-session');
 
 Feature('Co-respondent Adultery journey');
 
-Scenario('Proceed to adultery admission screen and admit adultery', I => {
-  I.amOnPage('/');
+Scenario('@Pipeline Proceed to adultery admission screen and admit adultery', async I => {
+  await I.createAUser();
+  I.createAosCaseForUser(adulteryDivorceSession);
+  await I.amOnLoadedPage('/');
 
   I.seeIdamLoginPage();
-  I.loginAsCorespondent();
-
+  await I.createAUser();
+  I.login();
+  I.seeCaptureCaseAndPinPage();
+  I.fillInReferenceNumberAndPinCode();
+  I.navByClick(content.en.continue);
+  if (config.tests.e2e.addWaitForCrossBrowser) {
+    I.wait(30);
+  }
   I.seeCrRespondPage();
   I.seeCrDocumentsForDownload();
   I.click(content.en.continue);
@@ -32,9 +42,19 @@ Scenario('Proceed to adultery admission screen and admit adultery', I => {
   I.consentToSendingCrNotifications();
   I.navByClick(content.en.continue);
 
+  if (config.features.respondentEquality === 'true') {
+    I.seeEqualityPage();
+    I.completePCQs();
+  }
+
+  I.wait(5);
+
   I.seeCrCheckYourAnswersPage();
   I.confirmInformationIsTrue();
   I.submitApplication();
+
+  I.seeDonePage();
+  I.see('LV17D80100');
 }).retry(2);
 
 Scenario('Proceed to adultery admission screen and do not admit adultery', I => {
