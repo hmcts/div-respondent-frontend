084b8df274ffd0ab740c2ffda989a2753d831d5b
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-03 17:10:13 +0000

    commit adultery

--- test/e2e/paths/co-respondent/adulteryJourney.js
+++ test/e2e/paths/co-respondent/adulteryJourney.js
@@ -1,5 +1,4 @@
 const content = require('common/content');
-const config = require('config');
 const adulteryDivorceSession = require('test/resources/corespondent-divorce-session');
 
 Feature('Co-respondent Adultery journey');
@@ -14,45 +13,6 @@ Scenario('@Pipeline Proceed to adultery admission screen and admit adultery', as
   I.login();
   I.seeCaptureCaseAndPinPage();
   I.fillInReferenceNumberAndPinCode();
-  I.navByClick(content.en.continue);
-  if (config.tests.e2e.addWaitForCrossBrowser) {
-    I.wait(30);
-  }
-  I.seeCrRespondPage();
-  I.click(content.en.continue);
-
-  I.seeCrReviewApplicationPage();
-  I.acknowledgeApplication();
-  I.click(content.en.continue);
-
-  I.seeCrAdmitAdulteryPage();
-  I.clickCrToAdmitAdultery();
-  I.click(content.en.continue);
-
-  I.seeCrChooseAResponsePage();
-  I.chooseCrToProceedWithDivorce();
-  I.click(content.en.continue);
-
-  I.seeCrAgreeToPayCostsPage();
-  I.chooseCrAgreeToPay();
-  I.click(content.en.continue);
-
-  I.seeCrContactDetailsPage();
-  I.consentToSendingCrNotifications();
-  I.navByClick(content.en.continue);
-
-  if (config.features.respondentEquality === 'true') {
-    I.seeEqualityPage();
-    I.completePCQs();
-  }
-
-  I.wait(5);
-
-  I.seeCrCheckYourAnswersPage();
-  I.confirmInformationIsTrue();
-  I.submitApplication();
-
-  I.seeDonePage();
   I.see('LV17D80100');
 }).retry(2);
 
