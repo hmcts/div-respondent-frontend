03de7b6679a5324f000d1bde476661c8ae6217e6
Rob Doherty <robdohertykelly@gmail.com>
2020-11-12 17:03:49 +0000

    fix: update supportedBrowsers.js to latest Mac and Windows OS

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -1,31 +1,34 @@
+const LATEST_MAC = 'macOS 10.15';
+const LATEST_WINDOWS = 'Windows 10';
+
 const supportedBrowsers = {
   microsoftIE11: {
     ie11: {
       browserName: 'internet explorer',
       name: 'IE11',
-      platform: 'Windows 10',
-      version: '11.285'
+      platform: LATEST_WINDOWS,
+      version: 'latest'
     }
   },
   microsoftEdge: {
     edge: {
       browserName: 'MicrosoftEdge',
       name: 'Edge_Win10',
-      platform: 'Windows 10',
-      version: '18.17763'
+      platform: LATEST_WINDOWS,
+      version: 'latest'
     }
   },
   chrome: {
     chrome_win_latest: {
       browserName: 'chrome',
       name: 'DIV_WIN_CHROME_LATEST',
-      platform: 'Windows 10',
+      platform: LATEST_WINDOWS,
       version: 'latest'
     },
     chrome_mac_latest: {
       browserName: 'chrome',
       name: 'MAC_CHROME_LATEST',
-      platform: 'macOS 10.13',
+      platform: LATEST_MAC,
       version: 'latest'
     }
   },
@@ -33,22 +36,22 @@ const supportedBrowsers = {
     firefox_win_latest: {
       browserName: 'firefox',
       name: 'WIN_FIREFOX_LATEST',
-      platform: 'Windows 10',
+      platform: LATEST_WINDOWS,
       version: 'latest'
     },
     firefox_mac_latest: {
       browserName: 'firefox',
       name: 'MAC_FIREFOX_LATEST',
-      platform: 'macOS 10.13',
+      platform: LATEST_MAC,
       version: 'latest'
     }
   },
   safari: {
     safari11: {
       browserName: 'safari',
-      name: 'DIV_SAFARI_11',
-      platform: 'macOS 10.13',
-      version: '11.1',
+      name: 'DIV_SAFARI_LATEST',
+      platform: LATEST_MAC,
+      version: 'latest',
       avoidProxy: true
     }
   }
