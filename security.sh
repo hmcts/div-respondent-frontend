7060934c9378c11a2ae8a0eee116fa8a17c1c5ee
Patryk Jar <jar.patryk@gmail.com>
2021-01-14 17:34:05 +0100

    RPET-656: bightly build update

--- security.sh
+++ security.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #echo "${SECURITYCONTEXT}" > /zap/security.context
-
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-
 zap-x.sh -d -host 0.0.0.0 -port 1001 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
 i=0
 while !(curl -s http://0.0.0.0:1001) > /dev/null
@@ -17,27 +13,21 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
   zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
   zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
-  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescanReport.xml -f xml
   echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
   chown -R $(id -u):$(id -u) activescan.html
-  chown -R $(id -u):$(id -u) activescanReport.xml
-
-  cp *.html functional-output/
-  cp activescanReport.xml functional-output/
-
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
   curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-  cp *.* functional-output/
+  cp *.html functional-output/
+  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
 
-echo
-echo ZAP Security vulnerabilities were found that were not ignored
-echo
-echo Check to see if these vulnerabilities apply to production
-echo and/or if they have fixes available. If they do not have
-echo fixes and they do not apply to production, you may ignore them
-echo
-echo To ignore these vulnerabilities, add them to:
-echo
-echo "./audit.json"
-echo
-echo and commit the change
+  echo
+  echo ZAP Security vulnerabilities were found that were not ignored
+  echo
+  echo Check to see if these vulnerabilities apply to production
+  echo and/or if they have fixes available. If they do not have
+  echo fixes and they do not apply to production, you may ignore them
+  echo
+  echo To ignore these vulnerabilities, add them to:
+  echo
+  echo "./audit.json"
+  echo
+  echo and commit the change
