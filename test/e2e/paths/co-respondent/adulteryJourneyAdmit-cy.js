b2e13a2a21a76ea3d8ed8b281ed6ae0635cbc3e7
Felix Eyetan <eyetanfelix@gmail.com>
2021-04-23 17:20:44 +0100

    e2e test fixes: Separating suites

---
+++ test/e2e/paths/co-respondent/adulteryJourneyAdmit-cy.js
@@ -0,0 +1,58 @@
+const content = require('common/content');
+const config = require('config');
+const adulteryDivorceSession = require('test/resources/corespondent-divorce-session');
+
+Feature('Co-respondent Adultery Journey');
+const language = 'cy';
+
+Scenario(`@Pipeline Proceed to adultery admission screen and admit adultery - ${language}`, async I => {
+  await I.retry(2).createAUser();
+  I.retry(2).createAosCaseForUser(adulteryDivorceSession, true);
+  await I.amOnLoadedPage('/', language);
+
+  await I.retry(2).createAUser();
+  I.login(language);
+  I.seeCaptureCaseAndPinPage(language);
+  I.fillInReferenceNumberAndPinCode();
+  I.navByClick(content[language].continue);
+  if (config.tests.e2e.addWaitForCrossBrowser) {
+    I.wait(3);
+  }
+
+  I.seeCrRespondPage(language);
+  I.click(content[language].continue);
+
+  I.seeCrReviewApplicationPage(language);
+  I.acknowledgeApplicationCr(language);
+  I.click(content[language].continue);
+
+  I.seeCrAdmitAdulteryPage(language);
+  I.clickCrToAdmitAdultery(language);
+  I.click(content[language].continue);
+
+  I.seeCrChooseAResponsePage(language);
+  I.chooseCrToProceedWithDivorce(language);
+  I.click(content[language].continue);
+
+  I.seeCrAgreeToPayCostsPage(language);
+  I.chooseCrAgreeToPay(language);
+  I.click(content[language].continue);
+
+  I.seeCrContactDetailsPage(language);
+  I.consentToSendingCrNotifications(language);
+  I.navByClick(content[language].continue);
+
+  if (config.features.respondentEquality === 'true') {
+    I.seeEqualityPage(language);
+    I.completePCQs(language);
+  }
+
+  I.wait(5);
+
+  I.seeCrCheckYourAnswersPage(language);
+  I.confirmInformationIsTrueCr(language);
+  I.submitApplicationCr(language);
+  I.seeDonePage(language);
+  I.see('EZ12D91234');
+})
+  .retry(2);
