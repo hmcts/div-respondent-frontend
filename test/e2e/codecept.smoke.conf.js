0326dcfa9b1e798b003b29a501a0f368cafafcd9
Felix Eyetan <eyetanfelix@gmail.com>
2021-04-27 10:11:51 +0100

    Update config to run all functional

--- test/e2e/codecept.smoke.conf.js
+++ test/e2e/codecept.smoke.conf.js
@@ -6,7 +6,7 @@ const waitForAction = config.tests.e2e.waitForAction;
 const chromeArgs = [ '--no-sandbox' ];
 
 exports.config = {
-  tests: './smoke/*.js',
+  tests: './paths/**/*.js',
   output: config.tests.e2e.outputDir,
   helpers: {
     Puppeteer: {
@@ -19,8 +19,10 @@ exports.config = {
         args: chromeArgs
       }
     },
+    IdamHelper: { require: './helpers/idamHelper.js' },
     JSWait: { require: './helpers/JSWait.js' }
   },
+  include: { I: './pages/steps.js' },
   mocha: {
     reporterOptions: {
       'codeceptjs-cli-reporter': {
@@ -31,6 +33,18 @@ exports.config = {
         stdout: '-',
         options: { mochaFile: `${config.tests.e2e.outputDir}/smoke-result.xml` }
       }
+    }
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true,
+      fullPageScreenshots: true
+    },
+    retryFailedStep: {
+      enabled: true
+    },
+    autoDelay: {
+      enabled: true
     }
   },
   name: 'Frontend Smoke Tests'
