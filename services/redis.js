690190e0b46ee93da83089c6b666a43f0bbfba2d
Ruban <ruban72@hotmail.com>
2021-12-15 10:49:39 +0000

    DFR-701: Fix Redis AAT Connectivity  (#599)
    * DFR-701: Redis setEnableOffline to true
    
    * DFR-701: Add Redis connection logging

--- services/redis.js
+++ services/redis.js
@@ -3,9 +3,13 @@ const config = require('config');
 const logger = require('services/logger').getLogger(__filename);
 
 const client = ioRedis.createClient(
-  process.env.REDIS_URL || config.services.redis.url,
-  { enableOfflineQueue: false }
+  process.env.REDIS_URL || config.services.redis.url
 );
+
+client.on('connect', () => {
+  logger.infoWithReq(null, 'redis_connect', 'Connected to Redis');
+});
+
 client.on('error', error => {
   logger.errorWithReq(null, 'redis_error', 'Error connecting to Redis', error);
 });
