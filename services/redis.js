0d170d2ccb4dcfd4f4f498a606e6310dceadeb1f
Ruban <ruban72@hotmail.com>
2021-12-15 09:16:59 +0000

    DFR-701: Add Redis connection logging

--- services/redis.js
+++ services/redis.js
@@ -5,6 +5,11 @@ const logger = require('services/logger').getLogger(__filename);
 const client = ioRedis.createClient(
   process.env.REDIS_URL || config.services.redis.url
 );
+
+client.on('connect', () => {
+  logger.infoWithReq(null, 'redis_connect', 'Connected to Redis');
+});
+
 client.on('error', error => {
   logger.errorWithReq(null, 'redis_error', 'Error connecting to Redis', error);
 });
