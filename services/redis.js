f9c660fab658a0dd0e255e8322ee3310ea024ea4
Ruban <ruban72@hotmail.com>
2021-12-15 08:13:40 +0000

    DFR-701: Redis setEnableOffline to true

--- services/redis.js
+++ services/redis.js
@@ -3,8 +3,7 @@ const config = require('config');
 const logger = require('services/logger').getLogger(__filename);
 
 const client = ioRedis.createClient(
-  process.env.REDIS_URL || config.services.redis.url,
-  { enableOfflineQueue: false }
+  process.env.REDIS_URL || config.services.redis.url
 );
 client.on('error', error => {
   logger.errorWithReq(null, 'redis_error', 'Error connecting to Redis', error);
