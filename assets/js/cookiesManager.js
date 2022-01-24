18b4340ed959f3b6403d534cdcb5d6dbbe894403
Barry Weeks <barry.weeks@justice.gov.uk>
2022-01-24 12:55:16 +0000

    Update cookiesManager.js
    Refactor setCookie() call

--- assets/js/cookiesManager.js
+++ assets/js/cookiesManager.js
@@ -42,11 +42,7 @@ function setCookiePreference() {
   // eslint-disable-next-line no-magic-numbers,no-use-before-define
   setCookie('cookies_preferences_set', true, expiryDays);
   // eslint-disable-next-line no-use-before-define
-  setCookie(
-    'cookies_policy',
-    `{"essential":true,"analytics":${getAnalyticsSelectedValue}, "apm:"${getApmSelectedValue}}`,
-    expiryDays
-  );
+  setCookie('cookies_policy', `{"essential":true,"analytics":${getAnalyticsSelectedValue}, "apm:"${getApmSelectedValue}}`, expiryDays);
   document.getElementById('cookie-preference-success').classList.remove('govuk-visually-hidden');
   if (document.getElementById('accept-all-cookies-successs')) {
     document.getElementById('accept-all-cookies-success').classList.add('govuk-visually-hidden');
