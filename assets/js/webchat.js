320626b2bfa61fbbd18d5349a3a1cc7aa57169cf
Barry Weeks <barry.weeks@justice.gov.uk>
2022-05-03 14:05:30 +0100

    Add webchat availability logic
    Add webchat availability logic

--- assets/js/webchat.js
+++ assets/js/webchat.js
@@ -17,6 +17,7 @@
     const button = document.querySelector('.chat-button');
     const webChat = document.querySelector('web-chat');
     const message = document.querySelector('#metrics');
+    const openHoursMessage = document.querySelector('#webchatHours');
 
     button.addEventListener('click', () => {
       windowOpener('/avaya-webchat', 'Web Chat - Divorce', 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=350,height=550,left=100,top=100');
@@ -26,21 +27,39 @@
       webChat.classList.add('hidden');
     });
 
+    // Clear webchat availability messages and hide button
+    const clearWebchatAvailabilityMessages = () => {
+      if (openHoursMessage !== null) {
+        openHoursMessage.classList.add('hidden');
+      }
+      message.innerHTML = '';
+      button.classList.add('hidden');
+    };
+
+    // Set initial state.  Should only be visible until JS downloads from webchat server.
+    if (openHoursMessage !== null) {
+      clearWebchatAvailabilityMessages();
+      message.innerHTML = 'Awaiting response from Webchat Server...';
+    }
+
     webChat.addEventListener('metrics', function(metrics) {
       const metricsDetail = metrics.detail;
       const ccState = metricsDetail.contactCenterState;
       const ewt = metricsDetail.ewt;
       const availableAgents = metricsDetail.availableAgents;
 
+      clearWebchatAvailabilityMessages();
+
       if (ccState !== 'Open') {
-        message.innerHTML = 'Web chat is now closed. Come back Monday to Friday 9am to 5pm. Or contact us using one of the ways below.';
-        button.classList.add('hidden');
+        if (openHoursMessage === null) {
+          message.innerHTML = 'Web chat is now closed. Come back Monday to Friday 9am to 5pm. Or contact us using one of the ways below.';
+        } else {
+          openHoursMessage.classList.remove('hidden');
+        }
       } else if (ewt < 300 && availableAgents > 0) {
-        message.innerHTML = '';
         button.classList.remove('hidden');
       } else {
         message.innerHTML = 'All our webchat QMCAs are busy helping other people. Please try again later or contact us using one of the ways below.';
-        button.classList.add('hidden');
       }
     });
 
