0661e1db10a66f4f29c1c581527237fae11d2938
Mike Lemos <gobii@me.com>
2023-06-26 15:03:22 +0100

    DFR-1199 upgrade to redis 6 (#732)
    * DFR-1199 upgrade to redis 6
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1199 removing the nvmrc warning
    
    * DFR-1199 upgrade azure
    
    * DFR-1199 upgrade azure
    
    * DFR-1199 upgrade terraform
    
    * DFR-1999 removing redis to get the one from shared
    
    * Bumping chart version/ fixing aliases
    
    * DFR-1999 removing redis to get the one from shared
    
    * DFR-1999 removing redis to get the one from shared
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,21 +25,6 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
   resource_group_name  = "core-infra-${var.env}"
 }
 
-resource "azurerm_key_vault_secret" "redis_connection_string" {
-  name = "${var.component}-redis-connection-string"
-  value = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
-  key_vault_id = data.azurerm_key_vault.div_key_vault.id
-}
-
-module "redis-cache" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = var.env != "preview" ? "${var.product}-redis" : "${var.product}-${var.reform_service_name}-redis"
-  location = var.location
-  env      = var.env
-  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
-  common_tags = var.common_tags
-}
-
 data "azurerm_key_vault" "div_key_vault" {
   name                = local.vaultName
   resource_group_name = local.vaultName
